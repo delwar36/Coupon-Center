3a0c8abadd581d778dc2bcb6d149c34ccdf20105
delwarh36 <delwarh543@gmail.com>
2019-09-18 12:22:01 +0600

    database write2

--- app/src/main/java/com/example/firstapp/activities/ProfileActivity.java
+++ app/src/main/java/com/example/firstapp/activities/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.example.firstapp.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -13,13 +14,23 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.firstapp.classes.User;
 import com.example.firstapp.fragments.ActivitiesFragment;
 import com.example.firstapp.fragments.OverviewFragment;
 import com.example.firstapp.fragments.TopFragment;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +42,8 @@ public class ProfileActivity extends AppCompatActivity
     int counter = 0;
     NavigationView proNavigationView;
     private FirebaseAuth firebaseAuth;
+    private DatabaseReference mDatabase;
+    private TextView pName, pEmail;
 
 
     @Override
@@ -39,6 +52,35 @@ public class ProfileActivity extends AppCompatActivity
         setContentView(R.layout.activity_profile);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        pName = findViewById(R.id.profName);
+        pEmail = findViewById(R.id.profEmail);
+
+
+        String userId = firebaseAuth.getUid();
+        assert userId != null;
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                assert user != null;
+                pName.setText(user.name);
+                pEmail.setText(user.email);
+                Log.w("RETRIEVED", user.name + " " + user.email);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("FAILED", "loadPost:onCancelled", databaseError.toException());
+                // [START_EXCLUDE]
+                Toast.makeText(ProfileActivity.this, "Failed to load post.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         Toolbar toolbar2 = findViewById(R.id.proToolbar);
         setSupportActionBar(toolbar2);
