6439a5c25ea816ed3dc7ca7e60f552e675b886bf
delwar <delwarh543@gmail.com>
2020-02-01 01:41:06 +0600

    affiliate added

--- app/src/main/java/com/example/firstapp/activities/AddAffiliateActivity.java
+++ app/src/main/java/com/example/firstapp/activities/AddAffiliateActivity.java
@@ -1,11 +1,17 @@
 package com.example.firstapp.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.example.firstapp.models.BestItemModel;
@@ -13,17 +19,69 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddAffiliateActivity extends AppCompatActivity {
 
-    EditText affTitle, affCategory, affThumbnail, affUrl, affOffer;
+    EditText affTitle, affThumbnail, affUrl, affOffer, getCate;
+    AutoCompleteTextView  affCategory;
     private Button submitBtn;
     private DatabaseReference mDatabase;
 
+    LinearLayout affLayout;
+
+    ArrayList<String> categories = new ArrayList<>();
+
+
+
+    String NODE = "offers";
+
+    RadioGroup btnGroup;
+
+
+
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_affiliate);
         setUpRef();
+        categories.add("Accessories");
+        categories.add("Gadgets");
+        categories.add("Food");
+
+        categories.add("Accessories");
+        categories.add("Gadgets");
+        categories.add("Food");
+
+        categories.add("Accessories");
+        categories.add("Gadgets");
+        categories.add("Food");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,categories);
+
+        affCategory.setAdapter(arrayAdapter);
+
+        affCategory.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus)
+                    affCategory.showDropDown();
+            }
+        });
+
+
+
+        affCategory.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                affCategory.showDropDown();
+                return false;
+            }
+        });
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +89,8 @@ public class AddAffiliateActivity extends AppCompatActivity {
                 writeNewAffiliate();
              }
         });
+
+
 
     }
 
@@ -43,6 +103,9 @@ public class AddAffiliateActivity extends AppCompatActivity {
         affUrl = findViewById(R.id.aff_url);
         affOffer = findViewById(R.id.aff_offer);
         submitBtn = findViewById(R.id.submit_btn);
+        btnGroup = findViewById(R.id.btnGroup);
+        affLayout = findViewById(R.id.affLayout);
+        getCate = findViewById(R.id.aff_get_cate);
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Affiliates");
     }
 
@@ -53,6 +116,7 @@ public class AddAffiliateActivity extends AppCompatActivity {
         String thumbnail = affThumbnail.getText().toString();
         String url = affUrl.getText().toString();
         String offer = affOffer.getText().toString();
+        String get = getCate.getText().toString();
 
         BestItemModel affiliate = new BestItemModel(thumbnail, category, title, url, offer);
 
@@ -60,7 +124,23 @@ public class AddAffiliateActivity extends AppCompatActivity {
             Toast.makeText(AddAffiliateActivity.this, "Fill up all data please", Toast.LENGTH_SHORT).show();
 
         } else {
-            mDatabase.child("offers").push().setValue(affiliate).addOnSuccessListener(new OnSuccessListener<Void>() {
+
+            if (NODE.equals("categories")){
+                Map<String, Object> cate = new HashMap<>();
+
+                cate.put("category", get);
+
+                mDatabase.child(NODE).push().setValue(cate).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        Toast.makeText(AddAffiliateActivity.this, "New Data Added Successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+
+            mDatabase.child(NODE).push().setValue(affiliate).addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
@@ -70,7 +150,30 @@ public class AddAffiliateActivity extends AppCompatActivity {
             });
         }
 
+    }
 
 
+    public void clickedRadioButton(View view) {
+        int btnId = btnGroup.getCheckedRadioButtonId();
+
+        switch (btnId){
+            case R.id.offerBtn:
+                affLayout.setVisibility(View.VISIBLE);
+                getCate.setVisibility(View.GONE);
+                NODE = "offers";
+                break;
+
+            case R.id.bannerBtn:
+                affLayout.setVisibility(View.VISIBLE);
+                getCate.setVisibility(View.GONE);
+                NODE = "banners";
+                break;
+
+            case R.id.catBtn:
+                affLayout.setVisibility(View.GONE);
+                getCate.setVisibility(View.VISIBLE);
+                NODE = "categories";
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/firstapp/activities/OrderConfirmationActivity.java
@@ -0,0 +1,13 @@
+package com.example.firstapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderConfirmationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_confirmation);
+    }
+}
